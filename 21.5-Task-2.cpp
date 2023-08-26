1e6d6ce3bacf8762998ff5daf02bde8f7f89891b
kirillsukhorukov <sukhorukov_kirill@mail.ru>
2023-08-27 00:47:22 +0700

    function check_int, input_int, input data added

--- 21.5-Task-2.cpp
+++ 21.5-Task-2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 //Перечисление построек
@@ -26,7 +27,7 @@ enum RoomType
 //Структура комнаты
 struct Room
 {
-    RoomType room;
+    RoomType roomType;
     int roomSquare;
 };
 
@@ -41,7 +42,7 @@ struct Floor
 //Структура строения
 struct Bilding
 {
-    int bildingType;
+    BildingType bildingType;
     int floorCount;
     int bildingSquare;
     bool stove;
@@ -57,8 +58,66 @@ int plotSquare=1;
 std::vector <Bilding> bilding();
 };
 
+//Функция проверки строки с целым числом
+bool check_int(std::string &num)
+{
+    //Проверка длины строки с числом
+    if (num.length() > 9)
+    {
+        std::cerr << std::endl << "Error! The number is not a positive integer!" << std::endl;
+        return false;
+    }
+    else 
+    {
+        //Проверка что все символы являются числами
+        for (int i = 0; i < num.length(); i++)
+        {
+            if (!isdigit(num[i]))
+                {
+                    std::cerr << std::endl << "Error! The number is not a positive integer!" << std::endl;
+                    return false;
+                }
+        }
+    }
+    return true;
+}
+
+int input_int (std::string str)
+{
+    std::string temp;
+    
+    do
+    {
+        std::cout << str;
+        std::getline(std::cin, temp);
+    } while (!check_int(temp));
+
+    int num = stoi (temp);
+    return num;
+}
+
+//Функция ввода данных о поселке
+void input_data(std::vector <Plot> &village)
+{
+    
+}
+
 int main()
 {
-    std::cout << "------ Village model ------" << std::endl;
+    std::cout << "------ Village model ------" << std::endl << std::endl;
+    
+    //Ввод количества участков
+    int plotCount = input_int("Enter the number of plots: ");
+
+    std::cout << plotCount << std::endl;
+
+    //Инициализация вектора поселка
+    std::vector <Plot> village(plotCount);
+    
+    //Ввод данных о поселке
+    input_data(village);
+    
+    std::cout << std::endl << "Program completed. Press any key...";
+    std::cin.get();
     return 1;
 }
