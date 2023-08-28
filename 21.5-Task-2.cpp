8a002d2c804c95f5be4e10b8a58cfb70bf8e019b
kirillsukhorukov <sukhorukov_kirill@mail.ru>
2023-08-29 00:30:04 +0700

    functios input village info, bilding type, room type added

--- 21.5-Task-2.cpp
+++ 21.5-Task-2.cpp
@@ -36,26 +36,26 @@ struct Floor
 {
     int roomCount;
     int ceilingHeight;
-    std::vector <Room> room();
+    std::vector <Room> rooms;
 };
 
 //Структура строения
 struct Bilding
 {
     BildingType bildingType;
-    int floorCount;
+    int floorCount=1;
     int bildingSquare;
-    bool stove;
-    std::vector <Floor> floor();
+    bool stove = false;
+    std::vector <Floor> floors;
 };
 
 //Структура участка
 struct Plot
 {
-int bildingCount=0;
-int plotNumber=1;
-int plotSquare=1;
-std::vector <Bilding> bilding();
+int bildingCount;
+int plotNumber;
+int plotSquare;
+std::vector <Bilding> bildings;
 };
 
 //Функция проверки строки с целым числом
@@ -96,10 +96,73 @@ int input_int (std::string str)
     return num;
 }
 
-//Функция ввода данных о поселке
-void input_data(std::vector <Plot> &village)
+//Функция ввода типа здания
+BildingType input_bilding_type ()
 {
-    
+    bool error = false;
+    std::string str;
+    do
+    {
+        error = false;
+        std::cout << "Enter the type of bilding (house, bath, garage, barn, paddock, veranda): ";
+        std::getline(std::cin,str);
+        if (str == "house") return house;
+        else if (str == "bath") return bath;
+        else if (str == "garage") return garage;
+        else if (str == "barn") return barn;
+        else if (str == "paddock") return paddock;
+        else if (str == "veranda") return veranda;
+        else 
+        {
+            std::cerr <<"Error! Repeat input." << std::endl;
+            error = true;
+        }
+    } while (error);
+}
+
+//Функция ввода типа комнаты
+RoomType input_room_type ()
+{
+    bool error = false;
+    std::string str;
+    do
+    {
+        error = false;
+        std::cout << "Enter the type of room (bedroom, kitchen, bathroom, childrens, living room): ";
+        std::getline(std::cin,str);
+        if (str == "bedroom") return bedroom;
+        else if (str == "kitchen") return kitchen;
+        else if (str == "bathroom") return bathroom;
+        else if (str == "childrens") return childrens;
+        else if (str == "living room") return livingRoom;
+        else 
+        {
+            std::cerr <<"Error! Repeat input." << std::endl;
+            error = true;
+        }
+    } while (error);
+}
+
+//Функция ввода данных о поселке
+void input_village_info(std::vector <Plot> &village)
+{
+    for (int v=0; v<village.size(); v++)
+    {
+        //Ввод данных об участке
+        std::cout << "--- Entering plot number " << v+1 << " information ---" << std::endl;
+        village[v].bildingCount = input_int("Enter the number of bildings: ");
+        village[v].plotNumber = v+1;
+        village[v].plotSquare = input_int("Enter the square of plot: ");
+
+        //Ввод данных о зданиях
+        for (int b=0; b<village[v].bildingCount; b++)
+        {
+            std::cout << "--- Entering bilding number " << b+1 << " information ---" << std::endl;
+            village[v].bildings[b].bildingType = input_bilding_type();
+            village[v].plotNumber = v+1;
+            village[v].plotSquare = input_int("Enter the square of plot: ");
+        }
+    }
 }
 
 int main()
@@ -109,13 +172,11 @@ int main()
     //Ввод количества участков
     int plotCount = input_int("Enter the number of plots: ");
 
-    std::cout << plotCount << std::endl;
-
     //Инициализация вектора поселка
     std::vector <Plot> village(plotCount);
     
     //Ввод данных о поселке
-    input_data(village);
+    input_village_info(village);
     
     std::cout << std::endl << "Program completed. Press any key...";
     std::cin.get();
