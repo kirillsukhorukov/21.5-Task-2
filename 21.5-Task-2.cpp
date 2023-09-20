6c8ffbb18b826053f46003547b0d08013a4b5584
kirillsukhorukov <sukhorukov_kirill@mail.ru>
2023-09-20 23:42:44 +0700

    development completed

--- 21.5-Task-2.cpp
+++ 21.5-Task-2.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <vector>
 
+int MAX_FLOORS = 3;
+int MIN_ROOMS = 2;
+int MAX_ROOMS = 4;
+
 //Перечисление построек
 enum BildingType
 {
@@ -35,7 +39,7 @@ struct Floor
 {
     Floor():rooms(roomCount){};
     int roomCount=1;
-    int ceilingHeight=100;
+    int ceilingHeight=250;
     std::vector <Room> rooms{};
 };
 
@@ -145,17 +149,36 @@ RoomType input_room_type ()
     } while (error);
 }
 
+//Функция ввода данных о наличии печи с трубой
+bool presence_stove()
+{
+    bool error = false;
+    std::string str;
+    do
+    {
+        error = false;
+        std::cout << "Does the building have a stove with a chimney? (Yes/No): ";
+        std::getline(std::cin,str);
+        if (str == "Yes") return true;
+        else if (str == "No") return false;
+        else 
+        {
+            std::cerr <<"Error! Repeat input." << std::endl;
+            error = true;
+        }
+    } while (error);
+}
+
 //Функция ввода данных о поселке
 void input_village_info(std::vector <Plot> &village)
 {
     for (int v=0; v<village.size(); v++)
     {
         //Ввод данных об участке
-        std::cout << "--- Entering plot number " << v+1 << " information ---" << std::endl;
+        std::cout << std::endl << "--- Entering plot number " << v+1 << " information ---" << std::endl;
         village[v].bildingCount = input_int("Enter the number of bildings: ");
         village[v].plotNumber = v+1;
-        village[v].plotSquare = input_int("Enter the square of plot: ");
-        std::cout <<std::endl;
+        village[v].plotSquare = input_int("Enter the square of plot (m^2): ");
 
         //Расширение вектора до размера, соответствующего количеству зданий на участке
         village[v].bildings.resize(village[v].bildingCount);
@@ -163,12 +186,59 @@ void input_village_info(std::vector <Plot> &village)
         //Ввод данных о зданиях
         for (int b=0; b<village[v].bildingCount; b++)
         {
-            std::cout << "--- Entering bilding number " << b+1 << " information ---" << std::endl;
-            village[v].bildings[b].bildingSquare = input_int("Enter the square of bilding: ");
+            std::cout << std::endl << "--- Entering bilding number " << b+1 << " information ---" << std::endl;
+            village[v].bildings[b].bildingSquare = input_int("Enter the square of bilding (m^2): ");
             village[v].bildings[b].bildingType = input_bilding_type();
-            std::cout <<std::endl;
+            //Ввод данных о наличии трубы в бане
+            if (village[v].bildings[b].bildingType == BildingType::bath) village[v].bildings[b].stove = presence_stove();
+            //Ввод данных о доме
+            if (village[v].bildings[b].bildingType == BildingType::house)
+            {
+                village[v].bildings[b].stove = presence_stove();
+                do
+                {
+                    village[v].bildings[b].floorCount = input_int("Enter the number of floors (from 1 to 3): ");
+                } while (village[v].bildings[b].floorCount>MAX_FLOORS);
+
+                //Расширение вектора до размера, соответствующего количеству этажей в доме
+                village[v].bildings[b].floors.resize(village[v].bildings[b].floorCount);
+
+                //Ввод данных о этаже
+                for (int f=0; f<village[v].bildings[b].floorCount; f++)
+                {
+                    std::cout << std::endl << "--- Entering floor number " << f+1 << " information ---" << std::endl;
+                    village[v].bildings[b].floors[f].ceilingHeight = input_int("Enter the height of the ceilings on the floor (cm): ");
+                    do
+                    {
+                        village[v].bildings[b].floors[f].roomCount = input_int("Enter the number of rooms (from 2 to 4): ");
+                    } while (village[v].bildings[b].floorCount<MIN_ROOMS || village[v].bildings[b].floorCount>MAX_ROOMS);
+
+                    //Расширение вектора до размера, соответствующего количеству комнат на этаже
+                    village[v].bildings[b].floors[f].rooms.resize(village[v].bildings[b].floors[f].roomCount);
+
+                    //Ввод данных о комнате
+                    for (int r=0; r<village[v].bildings[b].floors[f].roomCount; r++)
+                    {
+                        std::cout << std::endl << "--- Entering room number " << r+1 << " information ---" << std::endl;
+                        village[v].bildings[b].floors[f].rooms[r].roomSquare = input_int("Enter the square of room (m^2): ");
+                        village[v].bildings[b].floors[f].rooms[r].roomType = input_room_type();
+                    }
+                }
+            }
         }
     }
+}
+
+//Функция подсчета количества зданий в поселке
+void count_buildings(std::vector <Plot> &village)
+{
+    int count = 0;
+    for (int v=0; v<village.size(); v++)
+    {
+        count += village[v].bildingCount;
+    }
+
+    std::cout << std::endl << "Number of buildings in the village: " << count << std::endl;
 }
 
 int main()
@@ -181,10 +251,12 @@ int main()
 
     //Инициализация вектора поселка
     std::vector <Plot> village(plotCount);
-    //village.push_back();
     
     //Ввод данных о поселке
     input_village_info(village);
+
+    //Функция подсчета количества зданий в поселке
+    count_buildings(village);
     
     std::cout << std::endl << "Program completed. Press any key...";
     std::cin.get();
